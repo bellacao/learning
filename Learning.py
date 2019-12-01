a7d50b8a905241f19d5fee19270833d6497f2371
bellacao <bellacao2008@sina.com>
2019-12-01 20:56:29 +0800

    first learning python

--- Learning.py
+++ Learning.py
@@ -1,3 +1,17 @@
-print('pls enter the name')
-name = input()
-print(name,'is the good girl')
+# 这是第一堂课程，学习了变量 + 注释
+
+message = "hello python World"
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+message_01 = "20081210"
+
+x = 5 
+Y = 6.7
+
+message_02 = message + str(x) + message_01
+
+print(x+Y)
+
+print(message_02)
